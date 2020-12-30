287b4d22d3d881f2a462c0d6a7dbf65a616d6b6e
pcnerd37 <pcnerd37@globalgeeknews.com>
2020-12-29 20:43:51 -0700

    Create Project and Task classes.

---
+++ AchievementTracker/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AchievementTracker
+{
+    class Project
+    {
+        public string ProjectName { get; set; }
+        public int NumberOfTasks { get; set; }
+        public double ProjectDuration { get; set; }
+        public string ProjectStatus { get; set; }
+        public List<Task> ProjectTasks;
+        public DateTime DateCreated { get; set; }
+
+    }
+}
---
+++ AchievementTracker/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AchievementTracker
+{
+    class Task
+    {
+        public string taskName { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
